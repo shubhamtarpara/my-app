d58b002fd4e3539b00b7ad01191a7e1a76983c4c
shubhamtarpara <shubham.t@zuru.tech>
2022-06-14 18:06:57 +0530

    created app

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilterYear] = useState("2020");
+
+  const filterChangeHandler = (seletedYear) => {
+    setFilterYear(seletedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
